e7bed4e885f870d19e9ed44ac4b5086f8a8c23a1
Anass <146187264+nasaou@users.noreply.github.com>
2025-11-09 16:18:23 +0100

    Create layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'משרד עורכי דין דלתא',
-  description: 'אנחנו משרד עורכי דין מוביל בתחום הטכנולוגיה עם ניסיון של שנים רבות. אנחנו מתמחים במתן שירות מקצועי ואיכותי ללקוחותינו.',
+  description: 'אנחנו משרד עורכי דין מוביל בתחום החינוך עם ניסיון של שנים רבות. אנחנו מתמחים במתן שירות מקצועי ואיכותי ללקוחותינו.',
 };
 
 export default function RootLayout({
@@ -19,12 +18,11 @@ export default function RootLayout({
     <html lang="he" dir="rtl">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#588C7E" />
+        <meta name="theme-color" content="#4ECDC4" />
       </head>
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
           {/* Navbar will be inserted here */}
-          <Navbar />
 
           {children}
 
