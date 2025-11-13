db77a0f7759db9e378d31b2fffb6e0538bda22f4
Anass <146187264+nasaou@users.noreply.github.com>
2025-11-13 17:00:53 +0100

    Create tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,13 @@
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
       ],
-      'theme': {
-        'extend': {
-          'colors': {
-            primary: '#4ECDC4',
-            secondary: '#588C7E'
-          ,
-        background: "#0f172a"
-      },
-          'backgroundImage': {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#96CEB4',
+            secondary: '#FFEEAD'
+          },
+          backgroundImage: {
             'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
             'gradient-conic':
               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
